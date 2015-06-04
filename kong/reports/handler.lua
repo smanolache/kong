25cf3f60c368cff64f1f2ee6e169b1ebef5787af
thefosk <marco@mashape.com>
2015-06-04 01:30:38 -0700

    removing comment

--- kong/reports/handler.lua
+++ kong/reports/handler.lua
@@ -1,11 +1,3 @@
--- Kong resolver core-plugin
---
--- This core-plugin is executed before any other, and allows to map a Host header
--- to an API added to Kong. If the API was found, it will set the $backend_url variable
--- allowing nginx to proxy the request as defined in the nginx configuration.
---
--- Executions: 'access', 'header_filter'
-
 local BasePlugin = require "kong.plugins.base_plugin"
 local init_worker = require "kong.reports.init_worker"
 
